a2d9922ba9992f165a79a6c0e179e7f48bad9a66
Fabio Shinji Omine <fabio.omine21@gmail.com>
2023-12-18 18:26:08 -0300

    Projeto da Dio do Felipão

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-console.log("Olá Mundo!")
+let heroi = "Arthur"
+let xpHeroi = 7000
+let nvHeroi = "nível"
+
+if ( xpHeroi <= 1000){
+
+    nvHeroi = "Ferro"
+
+    console.log("O Herói de nome " + heroi + " está no nível de " + nvHeroi)
+
+}
+else if ((xpHeroi >= 1001) && (xpHeroi <= 2000)){
+
+    nvHeroi = "Bronze"
+
+    console.log("O Herói de nome " + heroi + " está no nível de " + nvHeroi)
+
+}
+else if ((xpHeroi >= 2001) && (xpHeroi <= 5000)){
+
+    nvHeroi = "Prata"
+
+    console.log("O Herói de nome " + heroi + " está no nível de " + nvHeroi)
+
+}
+else if((xpHeroi >= 6001) && (xpHeroi <= 7000)){
+
+    nvHeroi = "Ouro"
+
+    console.log("O Herói de nome " + heroi + " está no nível de " + nvHeroi)
+
+}
+else if ((xpHeroi >= 7001) && (xpHeroi <= 8000)){
+
+    nvHeroi = "Platina"
+
+    console.log("O Herói de nome " + heroi + " está no nível de " + nvHeroi)
+
+}
+else if ((xpHeroi >= 8001) && (xpHeroi <= 9000)){
+
+    nvHeroi = "Ascendente"
+
+    console.log("O Herói de nome " + heroi + " está no nível de " + nvHeroi)
+
+}
+else if ((xpHeroi >= 9001) && (xpHeroi <= 10000)){
+
+    nvHeroi = "Imortal"
+
+    console.log("O Herói de nome " + heroi + " está no nível de " + nvHeroi)
+
+}
+else{
+
+    nvHeroi = "Radiante"
+    
+    console.log("O Herói de nome " + heroi + " está no nível de " + nvHeroi)
+}
